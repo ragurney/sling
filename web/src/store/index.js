75a9aaf168935a4f20d1a854db3249c8e64a82ef
Ryan Gurney <ryan.a.gurney@gmail.com>
2017-08-20 22:06:23 -0700

    Implement base react app components

---
+++ web/src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from '../reducers';
+
+const middleWare = [thunk];
+const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+export default store;
